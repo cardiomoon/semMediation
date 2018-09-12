5423220321a9c1c739024c150440c5b645a58ee1
cardiomoon <cardiomoon@gmail.com>
2018-09-13 01:23:02 +0900

    2018-9-12
    2018-9-12

--- R/modmedEquation.R
+++ R/modmedEquation.R
@@ -1,19 +1,17 @@
-X="time.c";M="pubs";Y="jobs"
-moderator=list(name=c("alex.c"),site=list(c("a","c")))
-model=modmedEquation(X=X,M=M,Y=Y,moderator=moderator)
-cat(model)
-str(Success)
-model='pubs ~ a1*time.c+a2*alex.c+a3*alex.c:time.c
-jobs ~ c1*time.c+c2*alex.c+c3*alex.c:time.c + b1*pubs
-alex.c ~ alex.c.mean*1
-alex.c ~~ alex.c.var*alex.c'
-fit=sem(model,data=Success)
-parameterEstimates(fit)
-
-semDiagram(fit)
-conceptDiagram(fit)
-moderator
-require(stringr)
+# require(lavaan)
+# require(semMediation)
+# X="time.c";M="pubs";Y="jobs"
+# moderator=list(name=c("alex.c"),site=list(c("a","c")))
+# model=modmedEquation(X=X,M=M,Y=Y,moderator=moderator)
+# cat(model)
+# str(Success)
+# fit=sem(model,data=Success)
+# parameterEstimates(fit)
+#
+# semDiagram(fit)
+# conceptDiagram(fit)
+#
+# require(stringr)
 
 #'make interaction equation
 #'@param x character vector
@@ -37,6 +35,7 @@ interactStr=function(x,prefix="a"){
 #' Extract groupby string
 #' @param string character vector
 #' @param groupby name of groupby
+#' @importFrom stringr fixed
 extractX=function(string,groupby="X"){
    for(i in seq_along(string)){
       if(string[i]==groupby) string[i]=paste0("1*",groupby)
@@ -62,6 +61,7 @@ strGrouping=function(x,groupby="X"){
 #' @param Y A character vectors indicating dependent variables
 #' @param moderator moderator
 #' @param labels labels
+#' @importFrom stringr str_replace_all
 #' @export
 #' @examples
 #' X="X";M="M";Y="Y"
@@ -79,11 +79,15 @@ modmedEquation=function(X="",M="",Y="",moderator=list(),labels=NULL){
       MY=c(M,MY)
       XY=c(X,XY)
       MYstr=interactStr(MY,prefix="b")
+      MYstr
       XYstr=interactStr(XY,prefix="c")
+      XYstr
       temp=paste(Y,"~",stringr::str_flatten(XYstr,"+"),"+",
                  stringr::str_flatten(MYstr,"+"),"\n")
+      temp
       equation=paste0(equation,temp)
 
+      cat(equation)
       for(i in seq_along(moderator$name)){
         name=moderator$name[i]
         temp=paste0(name," ~ ",name,".mean*1\n")
@@ -91,21 +95,52 @@ modmedEquation=function(X="",M="",Y="",moderator=list(),labels=NULL){
         equation=paste0(equation,temp)
       }
 
-      XYstr1=stringr::str_replace_all(XYstr,":","*")
-      MEqui=stringr::str_flatten(XMstr,"+")
-      MYstr1=str_replace(MYstr,M,paste0("(",MEqui,")"))
-      MYstr1=stringr::str_replace_all(MYstr1,":","*")
-      MYstr2=removeParentheses(MYstr1)
-      totalStr=c(XYstr1,MYstr2)
-      result=strGrouping(totalStr,X)
-      direct=result$yes
-      indirect=result$no
+      XMstr=stringr::str_replace_all(XMstr,":","*")
+      ind1=strGrouping(XMstr,X)$yes
+      ind1
+      MYstr=stringr::str_replace_all(MYstr,":","*")
+      ind2=strGrouping(MYstr,M)$yes
+      ind2
+      ind=paste0("(",str_flatten(ind1,"+"), ")*(",str_flatten(ind2,"+"),")")
+      ind
+      ind.below<-ind.above<-ind
+      for(i in seq_along(moderator$name)){
+          temp=paste0("(",moderator$name[i],".mean-sqrt(",moderator$name[i],".var))")
+          ind.below=str_replace_all(ind.below,moderator$name[i],temp)
+          temp=paste0("(",moderator$name[i],".mean+sqrt(",moderator$name[i],".var))")
+          ind.above=str_replace_all(ind.above,moderator$name[i],temp)
+      }
+      ind.below
+      ind.above
+      equation=paste0(equation,"indirect.SDbelow:=",ind.below,"\n")
+      equation=paste0(equation,"indirect.SDabove:=",ind.above,"\n")
+      XYstr=stringr::str_replace_all(XYstr,":","*")
+      XYstr
+      direct=strGrouping(XYstr,X)$yes
+      dir=paste0(str_flatten(direct,"+"))
+      dir.below<-dir.above<-dir
+      for(i in seq_along(moderator$name)){
+          temp=paste0("(",moderator$name[i],".mean-sqrt(",moderator$name[i],".var))")
+          dir.below=str_replace_all(dir.below,moderator$name[i],temp)
+          temp=paste0("(",moderator$name[i],".mean+sqrt(",moderator$name[i],".var))")
+          dir.above=str_replace_all(dir.above,moderator$name[i],temp)
+      }
+      equation=paste0(equation,"direct.SDbelow:=",dir.below,"\n")
+      equation=paste0(equation,"direct.SDabove:=",dir.above,"\n")
+      equation=paste0(equation,"total.SDbelow := direct.SDbelow + indirect.SDbelow\n",
+                      "total.SDabove := direct.SDabove + indirect.SDabove\n")
+      equation=paste0(equation,"prop.mediated.SDbelow := indirect.SDbelow / total.SDbelow\n",
+                      "prop.mediated.SDabove := indirect.SDabove / total.SDabove\n")
 
-      equation=paste0(equation,"direct:=",str_flatten(direct,"+"),"\n")
-      equation=paste0(equation,"indirect:=",str_flatten(indirect,"+"),"\n")
-      equation=paste0(equation,"total:=direct+indirect\n")
+      if(length(moderator$name)==1) {
+          temp=ind1[str_detect(ind1,fixed("*"))]
+          temp=unlist(str_split(temp,fixed("*")))
+          temp[seq(1,length(temp),2)]
+          ind2
+          equation=paste0(equation,"index.mod.med := ",
+                          temp[seq(1,length(temp),2)],"*",str_flatten(ind2,"+"),"\n")
 
-
+      }
       equation
 }
 
